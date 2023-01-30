5cc530c1a0319e45dc35b9b071a3a3ad388e42ef
Axel Fiol <axelfiol12@gmail.com>
2023-01-30 16:15:38 -0300

    agrego js

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+/* function mostrar(titulo){
+    if (titulo.style.display == "none") {
+        titulo.style.display = "block"
+    }
+    else{
+        titulo.style.display = "none"
+    }
+
+
+        
+
+} */
+
+
+
+
+var mostrar= (titulo) => titulo.style.display=(titulo.style.display=="none") ? "block": "none"
