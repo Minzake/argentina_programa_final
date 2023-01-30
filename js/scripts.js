67b3dbfd4debbde153615e63847221dfb5dd3f45
Axel Fiol <axelfiol12@gmail.com>
2023-01-30 17:50:30 -0300

    finalizado

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,29 @@
-/* function mostrar(titulo){
-    if (titulo.style.display == "none") {
-        titulo.style.display = "block"
+
+/*
+arrow function con operador if ternario.
+oculta o muestra contenido de texto.
+*/
+
+var mostrar = (titulo) => titulo.style.display=(titulo.style.display=="none") ? "block" : "none";
+
+
+function cambiar_icon (icon,titulo) {
+    if (titulo.style.display == "none"){
+        icon.classList.remove("bi-caret-right");
+        icon.classList.add("bi-caret-down");
     }
     else{
-        titulo.style.display = "none"
+        icon.classList.remove("bi-caret-down");
+        icon.classList.add("bi-caret-right");
     }
 
+}
 
-        
+function hacer_click(titulo,icon){
+    cambiar_icon(icon,titulo);
+    mostrar(titulo);
+    
+}
 
-} */
-
-
-
-
-var mostrar= (titulo) => titulo.style.display=(titulo.style.display=="none") ? "block": "none"
+/* document.getElementById("container").classList.remove("color");
+document.getElementById('mi_elemento').classList.add('nueva_clase'); */
